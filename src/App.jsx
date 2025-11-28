3b661afa3fac8e9b33f2e59212aa739c1972390e
ashishajai2003 <ashishajai2003@gmail.com>
2025-11-28 12:45:55 +0530

    added a new button whose background color changes to grey

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [color3, setColor3] = useState("white");
+  function handleColor3() {
+    setColor3("Grey");
+  }
   return (
     <>
       <div>
@@ -21,6 +24,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button
+          onClick={handleColor3}
+          style={{ background: color3, color: "yellow" }}
+        >
+          Grey
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
