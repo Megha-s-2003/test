24fe675e68415e51df1553d8c30cd008dd5b8711
diyadilip <diyadilip213@gmail.com>
2025-11-28 12:45:56 +0530

    added a new button whose background color changes to pink

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [color2, setColor2] = useState("white");
+  function handleColor2() {
+    setColor2("Pink");
+  }
   return (
     <>
       <div>
@@ -21,6 +24,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button
+          onClick={handleColor2}
+          style={{ background: color2, color: "yellow" }}
+        >
+          Pink
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
